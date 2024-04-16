324442282fc264cd6dc045a071184434679758da
rahul-244 <77871021+rahul-244@users.noreply.github.com>
2024-04-16 08:46:16 -0400

    magic_tile_bot.py

--- magic_tile_bot.py
+++ magic_tile_bot.py
@@ -2,20 +2,22 @@ from pyautogui import *
 import pyautogui as p
 import time
 import keyboard
-import random
 import win32api, win32con
 
+#the coordinates of the center lines of the game
 # X:  288 Y:  340 RGB: (  0,   0,   0)
 # X:  366 Y:  340 RGB: (  0,   0,   0)
 # X:  455 Y:  340 RGB: (  0,   0,   0)
 # X:  521 Y:  340 RGB: (  0,   0,   0)
 
+#does a left click and then then leftup
 def click(x, y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(0.1)  # Increase to 0.1 seconds
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
+#this checks for the color black by checking when Red is 0 in RGB
 while keyboard.is_pressed('q') == False:
     if p.pixel(288, 340)[0] == 0:
         click(288, 340)
